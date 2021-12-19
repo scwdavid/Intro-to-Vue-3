257483710d7516ef82317fba6190b7f2e122c3d1
scwdavid <52196013+scwdavid@users.noreply.github.com>
2021-12-19 23:15:29 +0000

    up

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 const app = Vue.createApp({
     data() {
         return {
-            cart:0,
+            cart: 0,
             product: 'Socks',
             image: './assets/images/socks_blue.jpg',
             inStock: true,
@@ -10,6 +10,19 @@ const app = Vue.createApp({
               { id: 2234, color: 'green', image: './assets/images/socks_green.jpg' },
               { id: 2235, color: 'blue', image: './assets/images/socks_blue.jpg' },
             ]
+        }
+    },
+    methods: {
+        addToCart() {
+            this.cart += 1;
+        },
+        removeFromCart() {
+            if( this.cart >= 1 ) {
+                this.cart -= 1;
+            }
+        },
+        updateImage(variantImage) {
+            this.image = variantImage;
         }
     }
 })
